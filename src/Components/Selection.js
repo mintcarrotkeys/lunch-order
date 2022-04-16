1a152a0b1b836f5a3454e54345cc61a1929d5a12
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-04-16 12:58:45 +1000

    d

--- src/Components/Selection.js
+++ src/Components/Selection.js
@@ -88,7 +88,7 @@ export default function Selection(props) {
                 {errorMessage}
                 <div className="item-buttons">
                     <Button text="Cancel" type="grey" action={itemCancel} id={"cancelItem"} />
-                    <Button text="Add to order" type="green" action={itemSubmit} id={"submitItem"} />
+                    <Button text="Add item to order" type="green" action={itemSubmit} id={"submitItem"} />
                 </div>
             </div>
         )
@@ -132,7 +132,7 @@ export default function Selection(props) {
     let i = 0;
     for (const item of cart) {
         cartDisplay.push(
-            <div className="ticket-row" key={i}>
+            <div className="ticket-row cart-row" key={i}>
                 <div className="cart-left">
                     <h4 className="cart-name">
                         {menu[item.itemId].name}
@@ -147,7 +147,7 @@ export default function Selection(props) {
                     {"$" + menu[item.itemId].price.toFixed(2)}
                 </h4>
                 <div className="cart-delete" id={i} key={i} onClick={cartDelete}>
-                    <svg width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+                    <svg width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                     </svg>
                 </div>
