43ffe96a4d8996301ff74269f7db6a5f5143ba58
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-04-17 22:38:32 +1000

    Update Order.js

--- src/Pages/Order.js
+++ src/Pages/Order.js
@@ -107,7 +107,7 @@ export default function Order(props) {
 
             let changeDue = "";
             let changeGiven = "";
-            if (orderInfo.paid >= orderInfo.price) {
+            if (orderInfo.paid >= orderInfo.cost) {
                 changeGiven = (
                     <h4>
                         Change Given:
@@ -118,7 +118,7 @@ export default function Order(props) {
                     <h4>
                         Change Due:
                         <b>
-                            {"$" + (orderInfo.paid - orderInfo.price - orderInfo.change).toFixed(2)}
+                            {"$" + (orderInfo.paid - orderInfo.cost - orderInfo.change).toFixed(2)}
                         </b>
                     </h4>
                 )
@@ -151,7 +151,7 @@ export default function Order(props) {
                     <h2>Your Order</h2>
                     <p>You placed the order on: </p>
                     <h6>{formattedTime}</h6>
-                    <h4>Total Cost: <b>{"$" + orderInfo.price.toFixed(2)}</b></h4>
+                    <h4>Total Cost: <b>{"$" + orderInfo.cost.toFixed(2)}</b></h4>
                     <h4>Paid: <b>{"$" + orderInfo.paid.toFixed(2)}</b></h4>
                     {changeGiven}
                     {changeDue}
