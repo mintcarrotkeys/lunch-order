9585e45cf3a2159e107af87379e3110827d367f8
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-04-22 12:56:41 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,13 +87,19 @@ function App() {
     }
 
     return (
-        <div className="container">
-            <div className="card title">
-                <h1>e Lunch Order System</h1>
-                <h2><span style={{fontWeight: 300}}>Welcome:</span> {userName}</h2>
+        <div>
+            <div className="container">
+                <div className="card title">
+                    <h1>e Lunch Order System</h1>
+                    <h2><span style={{fontWeight: 300}}>Welcome:</span> {userName}</h2>
+                </div>
+                {pageBox}
             </div>
-            {pageBox}
+            <div className="card">
+                <p>Copyright (c) 2022 mintcarrotkeys <a href="https://github.com/mintcarrotkeys/lunch-order/blob/main/LICENSE">License</a></p>
+            </div>
         </div>
+
     );
 }
 
