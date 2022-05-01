9c641f889fed255328c967ba54efdeb92dc2f335
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-05-01 16:04:49 +1000

    Update Selection.js

--- src/Components/Selection.js
+++ src/Components/Selection.js
@@ -40,10 +40,7 @@ export default function Selection(props) {
     else {
         let sidesBox = "";
         let sides = [];
-        console.log(newItem);
         if (newItem.itemId !== "" && (menu[newItem.itemId].sides.length > 0)) {
-            console.log(menu[newItem.itemId].sides)
-            console.log(menu[newItem.itemId].sides.length)
             for (const side of menu[newItem.itemId].sides) {
                 sides.push(<option key={newItem.itemId + side} value={side}>{side}</option>);
             }
@@ -55,15 +52,10 @@ export default function Selection(props) {
                 </select>
             );
         }
-        else {
-            console.log("Hi")
-            console.log(sidesBox);
-            sidesBox = "";
-        }
         addItem = (
             <div className="ticket-row add-item-config">
                 <div className="item-topBox">
-                <select name="item-name" id="item-name" key={newItem.itemId} onChange={setItemName} defaultValue={newItem.itemId}
+                <select name="item-name" id="item-name" onChange={setItemName} defaultValue={newItem.itemId}
                         className="dropdown__selector item-name">
                     <option value={""}>choose item</option>
                     {options}
@@ -72,10 +64,8 @@ export default function Selection(props) {
                     {(newItem.itemId !== "" ? ("$" + menu[newItem.itemId].price.toFixed(2)) : " ")}
                 </div>
                 </div>
-                <div className="stack">
-                    {sidesBox}
-                </div>
-                <textarea type="text" id="item-notes" name="item-notes" key={newItem.itemId}
+                {sidesBox}
+                <textarea type="text" id="item-notes" name="item-notes"
                        className="input-text item-notes" maxLength={150}
                        placeholder="add special requirements here" spellCheck={true} autoCorrect={"on"}
                        onChange={setItemNotes} wrap="soft" rows={4}
@@ -142,7 +132,7 @@ export default function Selection(props) {
                 <h4 className="cart-price">
                     {"$" + menu[item.itemId].price.toFixed(2)}
                 </h4>
-                <div className="cart-delete" id={i} key={i} onClick={cartDelete}>
+                <div className="cart-delete" id={i} onClick={cartDelete}>
                     <svg width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                     </svg>
